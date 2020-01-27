3023c6b1da4074c2bdc7d28d4420625cebbd904c
Alejandro Mancilla <alejandro.mancilla.umsa@gmail.com>
2020-01-27 00:32:23 -0400

    manejo de ruta simple... saludo

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,9 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+
+
+Route::get('hola', function() {
+    return 'Hola Alejandro Mancilla';
+});
